09b802fec94adb574d83684e184bdcadf23255fb
AnthoniBortolotto <antho@outlook.com.br>
2023-03-22 21:39:36 -0300

    table html created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 "use client";
 import { automato, combinedTokens } from "@/helpers/syntaxAnaliser";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [table, setTable] = useState(automato("(5 + 5) * 4", combinedTokens));
 
-  useEffect(() => {
-    console.log("(5 + 5) * 4", automato("(5 + 5) * 4", combinedTokens));
-   // console.log("5 + a", automato("5 + a", combinedTokens));
-  }, []);
 
-  return <main>Oi</main>;
+
+  return (
+    <main>
+      <h1>Expressão: {"(D+,A,F,O,S)+"}</h1>
+      <h2>Entrada: {"(5 + 5) * 4"}</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Nome</td>
+            <td>Lexemas</td>
+            <td>Símbolo</td>
+          </tr>
+        </thead>
+        <tbody>
+          {table.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>
+                {item.lexema.map((lex, i) => {
+                  return <span key={i}>{lex} </span>;
+                })}
+              </td>
+              <td>{item.token}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 }
